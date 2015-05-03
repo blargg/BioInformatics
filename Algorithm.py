9fc7b9c430ec66713ea83692f8205395a57c48b1
blargg <anuibis@sbcglobal.net>
2015-05-02 23:28:03 -0500

    Added some algorithms

--- Algorithm.py
+++ Algorithm.py
@@ -17,3 +17,72 @@ def uniformRandomSampling(iterations, motifLength, sequences):
             bestSelection = selections
 
     return bestSelection
+
+
+def SGDpossibleMutations(maxIndex, currentSelection):
+    mutations = []
+    for i, v in enumerate(currentSelection):
+        assert v < maxIndex, "currentSelection is invalid"
+        for j in range(maxIndex):
+            if j is not v:
+                m = currentSelection[:]
+                m[i] = j
+                mutations.append(m)
+
+    return mutations
+
+
+def SGDimprove(evalPositions, motifLength, selections, sequences):
+    seqLen = len(sequences[0])
+    maxIndex = seqLen - motifLength
+    possiblePos = SGDpossibleMutations(maxIndex, selections)
+
+    return max(possiblePos, key=evalPositions)
+
+
+def SGD(iterations, motifLength, sequences):
+    """Selects a random assignment of positions, then repeadedly makes small
+    changes until it reaches a local maximum, then repeats"""
+    bestSelection = [0 for s in sequences]
+
+    def evalPositions(pos):
+        return M.infoInPositions(motifLength, sequences, pos)
+
+    for _ in range(iterations):
+        lastSelection = [random.randrange(len(s) - motifLength)
+                         for s in sequences]
+        currentSelection = SGDimprove(evalPositions, motifLength,
+                                      lastSelection, sequences)
+        while abs(evalPositions(currentSelection) -
+                  evalPositions(lastSelection)) < 0.000001:
+            assert len(lastSelection) == len(sequences),\
+                    "there should be exactly one selection per seq"
+            lastSelection = currentSelection
+            currentSelection = SGDimprove(evalPositions,
+                                          motifLength,
+                                          lastSelection,
+                                          sequences)
+            print(currentSelection)
+            print(evalPositions(currentSelection))
+        bestSelection = max(bestSelection, currentSelection, key=evalPositions)
+    return zip(sequences, bestSelection)
+
+
+def greedy(motifLength, sequences):
+
+    def evalWithSeq(seq):
+        return lambda pos: M.infoInPositions(motifLength, seq, pos)
+
+    maxIndex = len(sequences[0]) - motifLength
+    initialoptions = [[i, j] for i in range(maxIndex) for j in range(maxIndex)]
+    cumulative = max(initialoptions, key=evalWithSeq(sequences[:2]))
+
+    for i in range(2, len(sequences)):
+        options = []
+        for j in range(maxIndex):
+            copyList = cumulative[:]
+            copyList.append(j)
+            options.append(copyList)
+        cumulative = max(options, key=evalWithSeq(sequences[:i+1]))
+
+    return list(zip(sequences, cumulative))
